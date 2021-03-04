edff9b75ef74188737e0f03da84c5d4762ea3344
Lê Viết Hoàng <31377641+hashdotlee@users.noreply.github.com>
2021-03-04 16:49:06 +0000

    update #6 dcm

--- index.php
+++ index.php
@@ -50,13 +50,12 @@
         <!-- Optional JavaScript -->
         <script>
             var xhttp = new XMLHttpRequest();
-            let array = [];
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
                     var strJSON = this.responseText;
                     document.getElementById("json-encode").innerHTML = strJSON;
 
-                    array = JSON.parse(strJSON);
+                    var array = JSON.parse(strJSON);
                     document.getElementById("json-decode").innerHTML =
                         array[0]["name"];
                 }
