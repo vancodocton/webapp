54bc788eb319e5994dd23188a6cae59d565cca12
Lê Viết Hoàng <31377641+hashdotlee@users.noreply.github.com>
2021-03-04 16:14:29 +0000

    update #2

--- index.php
+++ index.php
@@ -70,7 +70,30 @@
                     </table>
                 );
             };
-            ReactDOM.render(<App />, document.getElementById("root"));
+			'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#root');ReactDOM.render(e(LikeButton), domContainer);
         </script>
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script
